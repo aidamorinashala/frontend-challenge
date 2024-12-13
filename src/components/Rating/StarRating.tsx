ae7e79a2390d2b370ea683a53520b5779ba79c31
Aida Shala <aida.morina-shala@capgemini.com>
2024-12-13 19:40:25 +0100

    update with interface structure instead

--- src/components/Rating/StarRating.tsx
+++ src/components/Rating/StarRating.tsx
@@ -1,6 +1,10 @@
 import { Star } from 'lucide-react';
 
-export const StarRating = (productRating: number) => {
+interface StarRatingProps {
+    productRating: number
+}
+
+export const StarRating = ({ productRating }: StarRatingProps) => {
   return (   
     <div className="flex items-center gap-1 mt-2">
       {[...Array(5)].map((_, i) => (
