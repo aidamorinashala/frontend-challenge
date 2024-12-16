55e3a9974515879109e4acb117443d60e48668d6
Aida Shala <aida.morina-shala@capgemini.com>
2024-12-16 09:09:09 +0100

    format file

--- src/components/CategoryLabel.tsx
+++ src/components/CategoryLabel.tsx
@@ -4,7 +4,10 @@ interface CategoryLabelProps {
 
 export const CategoryLabel = ({ category }: CategoryLabelProps) => {
   return (
-    <span className="inline-block bg-blue-100 text-sky-800 text-sm px-3 py-1 mb-2 rounded-full" aria-label="This item belongs to following category">
+    <span
+      className="inline-block bg-blue-100 text-sky-800 text-sm px-3 py-1 mb-2 rounded-full"
+      aria-label="This item belongs to following category"
+    >
       {category}
     </span>
   )
